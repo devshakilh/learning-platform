f9ed4bae137dc0e59f05925e1d1230ed47b627c0
shakilla1 <shakilshifat65@gmail.com>
2022-10-27 19:43:49 +0600

    comoplete githum login and google

--- src/pages/Login/Register.js
+++ src/pages/Login/Register.js
@@ -88,15 +88,7 @@ const Register = () => {
                     <Form.Control name="photoURL" type="text" placeholder="Phot URL" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control name="email" type="email" placeholder="Enter email" required />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control name="password" type="password" placeholder="Password" required />
-                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check
                         type="checkbox"
